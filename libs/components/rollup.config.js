f1943bb5d74af034c2eb09d97596191c1890d020
Löwe, Henning <henning.loewe@stud.th-luebeck.de>
2024-08-20 16:26:31 +0200

    chore: add rollup-plugin-import-css to separate css

--- libs/components/rollup.config.js
+++ libs/components/rollup.config.js
@@ -1,17 +1,19 @@
-const { withNx } = require('@nx/rollup/with-nx');
+const {withNx} = require('@nx/rollup/with-nx');
+import css from "rollup-plugin-import-css";
 
 module.exports = withNx(
-  {
-    main: './src/index.ts',
-    outputPath: '../../dist/libs/components',
-    tsConfig: './tsconfig.lib.json',
-    compiler: 'swc',
-    format: ['cjs', 'esm'],
-    assets: [{ input: '.', output: '.', glob: '*.md' }],
-  },
-  {
-    // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
-    // e.g.
-    // output: { sourcemap: true },
-  }
+    {
+        main: './src/index.ts',
+        outputPath: '../../dist/libs/components',
+        tsConfig: './tsconfig.lib.json',
+        compiler: 'swc',
+        format: ['cjs', 'esm'],
+        assets: [{input: '.', output: '.', glob: '*.md'}],
+    },
+    {
+        plugins: [css()]
+        // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
+        // e.g.
+        // output: { sourcemap: true },
+    }
 );
